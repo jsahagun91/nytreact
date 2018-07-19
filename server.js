a47be25b066763538c73c4b703c52319fcfa0655
Jose Sahagun <JoseSahagun@Joses-MacBook-Pro-2.local>
2018-07-19 01:19:23 -0700

    deployment try no. 2

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/public"));
+    app.use(express.static("client/build"));
 }
+
+app.get("*", (req, res) => {  
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 // Add routes, both API and view
 app.use(routes);
 
